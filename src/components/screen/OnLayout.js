8ac4a430d2b10ddc0f76cdbf21580090ec13bb1a
AddChan <is528629@gmail.com>
2019-12-28 20:59:25 +0900

    OnLayout Component Commit

---
+++ src/components/screen/OnLayout.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+class OnLayout extends Component {
+  state = {
+    width: 0,
+    height: 0,
+    x: 0,
+    y: 0
+  };
+
+  onLayout = e => {
+    this.setState({
+      width: e.nativeEvent.layout.width,
+      height: e.nativeEvent.layout.height,
+      x: e.nativeEvent.layout.x,
+      y: e.nativeEvent.layout.y
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container} onLayout={this.onLayout}>
+        <Text style={styles.text}>
+          width: {this.state.width}
+          {`\n`}
+          height: {this.state.height}
+          {`\n`}
+          x: {this.state.x}
+          {`\n`}
+          y: {this.state.y}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 300,
+    height: 300,
+    backgroundColor: "yellowgreen",
+    marginTop: 100,
+    marginLeft: 50
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold"
+  }
+});
+
+export default OnLayout;
