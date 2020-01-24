9a16b3116e49a9eef0f184b5d117b973f07a0c1e
AddChan <is528629@gmail.com>
2020-01-24 15:07:15 +0900

    SafeAreaView Component Update

--- src/components/screen/SafeAreaView.js
+++ src/components/screen/SafeAreaView.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 
+// 아이폰X 부터는 노치 디자인이 적용되어, 컴포넌트 생성시 이상해집니다.
+// 따라서 SafeAreaView를 사용하여, 노치 디자인에 의해 안 보이는 영역으로 컴포넌트가 렌더링되지 않도록 해주는 것입니다.
+
 class SafeAreaViewExample extends Component {
   render() {
     return (
