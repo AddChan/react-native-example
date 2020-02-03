cac38de55acca638e481f727ea3aacef0a175f4f
AddChan <is528629@gmail.com>
2020-02-03 22:33:33 +0900

    KeyboardingAvoidingView Component Update

--- src/components/screen/KeyboardAvoidingView.js
+++ src/components/screen/KeyboardAvoidingView.js
@@ -1,25 +1,42 @@
-//import liraries
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-// create a component
 const KeyboardAvoidingView = () => {
+  state = {
+    text: ""
+  };
   return (
-    <View style={styles.container}>
-      <Text>KeyboardAvoidingView</Text>
-    </View>
+    <KeyboardAvoidingView behavior="padding" style={styles.form}>
+      <TextInput
+        style={styles.input}
+        value={this.state.text}
+        onChangeText={text => this.setState({ text })}
+        placeholder="text"
+      />
+      <View>
+        <Text>KeyboardAvoidingView Example</Text>
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#2c3e50"
+  },
+  input: {
+    margin: 20,
+    marginBottom: 0,
+    height: 34,
+    paddingHorizontal: 10,
+    borderRadius: 4,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    fontSize: 16
   }
 });
 
-//make this component available to the app
 export default KeyboardAvoidingView;
