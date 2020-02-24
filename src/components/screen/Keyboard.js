173863312bb69bd31cd424889434296b4fc75b0a
AddChan <is528629@gmail.com>
2020-02-24 19:12:43 +0900

    Keyboard Component Update

--- src/components/screen/Keyboard.js
+++ src/components/screen/Keyboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Keyboard, TextInput } from "react-native";
 
+// KeyboardExample 대한 설명 블로그는 추후 업로드 될 예정입니다.
 export default class KeyboardExample extends Component {
   componentDidMount() {
     this.keyboardDidShowListener = Keyboard.addListener(
@@ -19,11 +20,11 @@ export default class KeyboardExample extends Component {
   }
 
   _keyboardDidShow() {
-    alert("Keyboard Shown");
+    alert("키보드 On!");
   }
 
   _keyboardDidHide() {
-    alert("Keyboard Hidden");
+    alert("키보드 Off!");
   }
 
   render() {
